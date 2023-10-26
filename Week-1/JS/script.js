05230aff165e275ee22a19c0f059fff459a6af25
gustitreshana <gustith2105@gmail.com>
2023-10-27 01:44:46 +0700

    add responsiv, add hamburger button, add animation on hamburger button

--- Week-1/JS/script.js
+++ Week-1/JS/script.js
@@ -9,3 +9,15 @@ function onClick2() {
 function btnSubmit() {
     alert("Maaf, saat ini fungsi tombol belum berfungsi dengan baik")
 }
+
+function burger(x) {
+    x.classList.toggle("change")
+}
+
+const hamburger = document.querySelector(".hamburger")
+const rightNav = document.querySelector(".text-right-nav")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    rightNav.classList.toggle("active");
+})
